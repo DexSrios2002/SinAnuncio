54cc82beb8b5642d3c2cd3bdb6de2e12212598b1
DexSrios2002 <168986479+DexSrios2002@users.noreply.github.com>
2024-05-05 11:16:56 -0600

    Update BloAnuncio.js

--- BloAnuncio.js
+++ BloAnuncio.js
@@ -6,8 +6,8 @@
 // @author       Donny
 // @match        https://www.youtube.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
-// @updateURL    https://github.com/TheRealJoelmatic/RemoveAdblockThing/raw/main/Youtube-Ad-blocker-Reminder-Remover.user.js
-// @downloadURL  https://github.com/TheRealJoelmatic/RemoveAdblockThing/raw/main/Youtube-Ad-blocker-Reminder-Remover.user.js
+// @updateURL    https://github.com/DexSrios2002/SinAnuncio/edit/main/BloAnuncio.js
+// @downloadURL  https://github.com/DexSrios2002/SinAnuncio/edit/main/BloAnuncio.js
 // @grant        none
 // ==/UserScript==
 
